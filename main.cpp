75fdf577ac64f330884e7e9747205f1c7b53e966
Owen <owen@ownwn.com>
2025-11-14 16:18:10 +1300

    owen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "owen.h"
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include "user.h"
+
 int main() {
     std::cout << "Hello, World !" << hey() << std::endl;
+    User a(10);
+    std::cout << "user has rating " << a.get_ratings().at(0).to_string() << '\n';
     return 0;
 }
